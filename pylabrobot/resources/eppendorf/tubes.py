7004539ab59b173e35918e6941f2253077435c7d
Scott Lee <scottslee@google.com>
2025-06-27 16:21:43 +0000

    Adding 1.5mL and 5.0mL tubes

--- pylabrobot/resources/eppendorf/tubes.py
+++ pylabrobot/resources/eppendorf/tubes.py
@@ -17,3 +17,41 @@ def Eppendorf_DNA_LoBind_1_5ml_Vb(name: str) -> Tube:
     model="Eppendorf_DNA_LoBind_1_5ml_Vb",
     max_volume=1_400,  # units: ul
   )
+
+
+
+def Eppendorf_1_5ml_Vb(name: str) -> Tube:
+  """1.5 mL round-bottom snap-cap Eppendorf tube. cat. no.: 022431021
+
+  - bottom_type=TubeBottomType.V
+  - snap-cap lid
+  """
+  # material_z_thickness = 2.4 mm
+  diameter = 10.6
+  return Tube(
+    name=name,
+    size_x=diameter,
+    size_y=diameter,
+    size_z=38.6,
+    model="Eppendorf_1_5ml_Vb",
+    max_volume=1_500,  # units: ul
+  )
+
+
+
+def Eppendorf_5ml_Vb(name: str) -> Tube:
+  """1.5 mL round-bottom snap-cap Eppendorf tube. cat. no.: 022431021
+
+  - bottom_type=TubeBottomType.V
+  - snap-cap lid
+  """
+  # material_z_thickness = 2.4 mm
+  diameter = 16.5
+  return Tube(
+    name=name,
+    size_x=diameter,
+    size_y=diameter,
+    size_z=56,
+    model="Eppendorf_5ml_Vb",
+    max_volume=5_000,  # units: ul
+  )

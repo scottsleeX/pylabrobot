eb707bf516c569288b22092f807e7b3a24cfecc0
Scott Lee <scottslee@google.com>
2025-06-27 03:20:11 +0000

    Adding Trough_CAR_5R60_A00

--- pylabrobot/resources/hamilton/trough_carriers.py
+++ pylabrobot/resources/hamilton/trough_carriers.py
@@ -39,3 +39,38 @@ def Trough_CAR_4R200_A00(name: str) -> TroughCarrier:
     ),
     model="Trough_CAR_4R200_A00",
   )
+
+def Trough_CAR_5R60_A00(name: str) -> TroughCarrier:
+  """Hamilton cat. no.: 185436
+  Hamilton name: 'RGT_CAR_4R200_A00'.
+  Trough carrier for 4x 200ml troughs. 2 tracks(T) wide.
+  - Material: ? (recognisable via cLLD)
+  - Sterilization_compatibility: ?
+
+  carrier_site_pedestal_top = 134
+  carrier_site_pedestal_bottom = 132.5
+  pedestal_z_height = 1.5
+  true_dz = 1.2
+  trought_z_thickness = 1.4
+  """
+  return TroughCarrier(
+    name=name,
+    size_x=45.0,
+    size_y=497.0,
+    size_z=71.5,
+    # pedestal_size_z=1.5 mm
+    sites=create_homogeneous_resources(
+      klass=ResourceHolder,
+      locations=[
+        Coordinate(4.0, 2.0, 34.0 + 1.5),
+        Coordinate(4.0, 99.06, 34.0 + 1.5),  # TODO: properly define troughs to remove dependency
+        Coordinate(4.0, 196.1, 34.0 + 1.5),  # on this 1.5mm offset (material_z_thickness)
+        Coordinate(4.0, 293.2, 34.0 + 1.5),
+        Coordinate(4.0, 390.3, 34.0 + 1.5),
+      ],
+      resource_size_x=37.0,
+      resource_size_y=118.0,
+      name_prefix=name,
+    ),
+    model="Trough_CAR_5R60_A00",
+  )

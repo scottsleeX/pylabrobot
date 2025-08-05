8fb28b81bf66985e6661aed45cdb2e66a1ed1013
Scott Lee <scottslee@google.com>
2025-08-05 22:35:59 +0000

    Adding 5mL tube v <--> h

--- pylabrobot/resources/eppendorf/tubes.py
+++ pylabrobot/resources/eppendorf/tubes.py
@@ -3,58 +3,65 @@ import math
 
 
 def _compute_volume_from_height_Eppendorf_1_5mL_Vb(h: float) -> float:
-    R =  10.5/2
-    Hk = 20.
-    Hc = 16.
+    a, b, c, d, e = -4.557517e-03, 2.866057e-01, -3.987186e+00, 3.075703e+01, -2.834161e+00
+    V = a * h**4 + b * h**3 + c * h**2 + d * h + e
+    return round(V,3)
+    # R =  10.5/2
+    # Hk = 20.
+    # Hc = 16.
 
-    total_height = Hk + Hc
-    if not (0 <= h <= total_height):
-        raise ValueError(f"Height must be between 0 and {total_height}")
+    # total_height = Hk + Hc
+    # if not (0 <= h <= total_height):
+    #     raise ValueError(f"Height must be between 0 and {total_height}")
 
-    # Case 1: Liquid is in the conical part (0 <= h <= Hk)
-    if h <= Hk:
-        volume = (math.pi * R**2 * h**3) / (3 * Hk**2)
-        print(f"input height < cone height: volume={volume}")
-        return round(volume,3)
+    # # Case 1: Liquid is in the conical part (0 <= h <= Hk)
+    # if h <= Hk:
+    #     volume = (math.pi * R**2 * h**3) / (3 * Hk**2)
+    #     print(f"input height < cone height: volume={volume}")
+    #     return round(volume,3)
 
-    # Case 2: Liquid is in the cylindrical part (h > Hk)
-    else:
-        volume_cone = (1/3) * math.pi * R**2 * Hk
-        volume_in_cylinder = math.pi * R**2 * (h - Hk)
-        print(f"input height > cone height: height={volume_cone+volume_in_cylinder}")
-        return round(volume_cone + volume_in_cylinder,3)
+    # # Case 2: Liquid is in the cylindrical part (h > Hk)
+    # else:
+    #     volume_cone = (1/3) * math.pi * R**2 * Hk
+    #     volume_in_cylinder = math.pi * R**2 * (h - Hk)
+    #     print(f"input height > cone height: height={volume_cone+volume_in_cylinder}")
+    #     return round(volume_cone + volume_in_cylinder,3)
 
 def _compute_height_from_volume_Eppendorf_1_5mL_Vb(V: float) -> float:
-    R =  10.5/2
-    Hk = 20.
-    Hc = 16.
+    a, b, c, d, e = -1.462645e+01, 6.252253e+01, -9.364077e+01, 6.977987e+01, 9.578706e-01
+    V /= 1000.
+    V = a * V**4 + b * V**3 + c * V**2 + d * V + e
+    return round(V,3)
+    # R =  10.5/2
+    # Hk = 20.
+    # Hc = 16.
 
-    volume_cone = (1/3) * math.pi * R**2 * Hk
-    volume_cylinder = math.pi * R**2 * Hc
-    total_volume = volume_cone + volume_cylinder
+    # volume_cone = (1/3) * math.pi * R**2 * Hk
+    # volume_cylinder = math.pi * R**2 * Hc
+    # total_volume = volume_cone + volume_cylinder
 
-    if not (0 <= V <= total_volume):
-        raise ValueError(f"Volume must be between 0 and {total_volume:.2f}")
+    # if not (0 <= V <= total_volume):
+    #     raise ValueError(f"Volume must be between 0 and {total_volume:.2f}")
 
-    # Case 1: Liquid is in the conical part (V <= Vk)
-    if V <= volume_cone:
-        # Using the rearranged formula h = Hk * (V / Vk)^(1/3) for stability
-        height = Hk * (V / volume_cone)**(1/3)
-        print(f"input volume < cone: height={height}")
-        return round(height,3)
+    # # Case 1: Liquid is in the conical part (V <= Vk)
+    # if V <= volume_cone:
+    #     # Using the rearranged formula h = Hk * (V / Vk)^(1/3) for stability
+    #     height = Hk * (V / volume_cone)**(1/3)
+    #     print(f"input volume < cone: height={height}")
+    #     return round(height,3)
 
-    # Case 2: Liquid is in the cylindrical part (V > Vk)
-    else:
-        volume_in_cylinder = V - volume_cone
-        height_in_cylinder = volume_in_cylinder / (math.pi * R**2)
-        print(f"input volume > cone: height={height}")
-        return round(Hk + height_in_cylinder,3)
+    # # Case 2: Liquid is in the cylindrical part (V > Vk)
+    # else:
+    #     volume_in_cylinder = V - volume_cone
+    #     height_in_cylinder = volume_in_cylinder / (math.pi * R**2)
+    #     print(f"input volume > cone: height={height}")
+    #     return round(Hk + height_in_cylinder,3)
 
 
 
 
 def _compute_volume_from_height_Eppendorf_5mL_Vb(h: float) -> float:
-    a, b, c, d, e = -2.114710e-03, 1.464585e-01, -3.978945e-01, 1.622262e+01, 1.447176e+01
+    a, b, c, d, e = -2.085268e-03, 1.515189e-01, -7.119490e-01, 1.517639e+01, 1.040188e+01
     V = a * h**4 + b * h**3 + c * h**2 + d * h + e
     return round(V,3)
     # R =  16.5/2.
@@ -79,7 +86,7 @@ def _compute_volume_from_height_Eppendorf_5mL_Vb(h: float) -> float:
     #     return round(volume_cone + volume_in_cylinder,3)
 
 def _compute_height_from_volume_Eppendorf_5mL_Vb(V: float) -> float:
-    a, b, c, d, e, f = 1.909598e-01, -2.558894e+00, 1.293383e+01, -3.054189e+01, 4.003277e+01, 4.039098e-01
+    a, b, c, d, e, f = 2.253775e-01, -2.978262e+00, 1.478127e+01, -3.407929e+01, 4.278537e+01, 7.857063e-01
     V /= 1000.
     height = a * V**5 + b * V**4 + c * V**3 + d * V**2 + e * V + f
     return round(height,3)
@@ -138,7 +145,7 @@ def Eppendorf_1_5ml_Vb(name: str) -> Tube:
   - bottom_type=TubeBottomType.V
   - snap-cap lid
   """
-  material_z_thickness = 1
+  material_z_thickness = 1.
   diameter = 10.6
   return Tube(
     name=name,
@@ -160,8 +167,7 @@ def Eppendorf_5ml_Vb(name: str) -> Tube:
   - bottom_type=TubeBottomType.V
   - snap-cap lid
   """
-  # material_z_thickness = 2.4 mm
-  material_z_thickness = 1
+  material_z_thickness = 1.2
   diameter = 16.5
   return Tube(
     name=name,

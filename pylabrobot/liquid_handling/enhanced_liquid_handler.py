841e3c71569a195c6f7b0f17d0531742f1f28aef
scottslee@google.com <scottslee@google.com>
2025-10-30 04:50:24 +0000

    Removing setup`

--- pylabrobot/liquid_handling/enhanced_liquid_handler.py
+++ pylabrobot/liquid_handling/enhanced_liquid_handler.py
@@ -91,7 +91,7 @@ class EnhancedLiquidHandler(LiquidHandler):
   async def setup(self):
     """ Asynchronously setup the list of available tips from the tip racks on the deck. """
     self._sync_setup()
-    await super().setup()
+    # await super().setup()
 
   async def pick_up_tips(
     self,
